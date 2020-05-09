d81e7c6093fb23f1797f34b58a655c42ae8bee82
santiago3028 <santico3028@gmail.com>
2020-05-08 23:38:37 -0500

    Solucion requerimiento

--- Colors/app/src/main/java/com/example/colors/HelpActivity.java
+++ Colors/app/src/main/java/com/example/colors/HelpActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AboutofActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aboutof);
+        setContentView(R.layout.activity_help);
 
         //activa la flecha en el actionbar
         assert getSupportActionBar() != null;   //null check
--- Colors/app/src/main/java/com/example/colors/Palette.java
+++ Colors/app/src/main/java/com/example/colors/Palette.java
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Palette extends AppCompatActivity
@@ -22,12 +24,14 @@ public class Palette extends AppCompatActivity
     private SeekBar vBlue = null;
     private SeekBar vAlpha = null;
     private View vFilter = null;
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
 
+        message = findViewById(R.id.txtMessage);
         //Code here .....
         //Get components' id's
         vRed = findViewById(R.id.sbrRed);
@@ -40,10 +44,13 @@ public class Palette extends AppCompatActivity
         vGreen.setOnSeekBarChangeListener(this);
         vBlue.setOnSeekBarChangeListener(this);
         vAlpha.setOnSeekBarChangeListener(this);
+
+        //Show the context menu WHEN I do a long press in the component
+        registerForContextMenu(vFilter);
     }
 
     //#############################################
-    //MENUS
+    //OPTIONS MENU
     //#############################################
     //Show the options menu on the Device.
     @Override
@@ -57,27 +64,51 @@ public class Palette extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.iteTransparent :
-                vAlpha.setProgress(0);
-                //Toast.makeText(this, "This color is going to change", Toast.LENGTH_SHORT).show();
+
+            case R.id.icHelp :
+                Intent intent = new Intent(this, HelpActivity.class);
+                startActivity(intent);
                 break;
+
+            case R.id.icTransparent :
+
+                vAlpha.setProgress(0);
+                break;
+
+            case R.id.iteTransparent :
+                Toast.makeText(this, "This color is going to change", Toast.LENGTH_SHORT).show();
+                vAlpha.setProgress(0);
+                message.setText("Transparent ");
+                break;
+            //Toast.makeText(this, "This color is going to change", Toast.LENGTH_SHORT).show();
 
             case R.id.iteSemitransparent :
                 vAlpha.setProgress(128);
                 vRed.setProgress(0);
                 vGreen.setProgress(0);
                 vBlue.setProgress(0);
+                message.setText("Semitransparent");
                 break;
 
             case R.id.iteOpaque :
+                vRed.setProgress(0);
+                vGreen.setProgress(0);
+                vBlue.setProgress(0);
                 vAlpha.setProgress(255);
+                message.setText("Opaque");
                 break;
 
             case R.id.iteBlack :
+                vAlpha.setProgress(255);
+                message.setText("Black");
                 break;
 
             case R.id.iteWhite :
-                //Code to change color
+                vRed.setProgress(255);
+                vGreen.setProgress(255);
+                vBlue.setProgress(255);
+                vAlpha.setProgress(128);
+                message.setText("White");
                 break;
 
             case R.id.iteRed :
@@ -85,6 +116,7 @@ public class Palette extends AppCompatActivity
                 vGreen.setProgress(0);
                 vBlue.setProgress(0);
                 vAlpha.setProgress(128);
+                message.setText("Red");
                 break;
 
             case R.id.iteGreen :
@@ -93,6 +125,7 @@ public class Palette extends AppCompatActivity
                 vGreen.setProgress(255);
                 vBlue.setProgress(0);
                 vAlpha.setProgress(128);
+                message.setText("Green");
                 break;
 
             case R.id.iteBlue :
@@ -101,29 +134,94 @@ public class Palette extends AppCompatActivity
                 vGreen.setProgress(0);
                 vBlue.setProgress(255);
                 vAlpha.setProgress(128);
+                message.setText("Blue");
                 break;
 
             case R.id.iteCyan :
+                vRed.setProgress(0);
+                vGreen.setProgress(160);
+                vBlue.setProgress(255);
+                vAlpha.setProgress(128);
+                message.setText("Cyan");
                 break;
 
             case R.id.iteMagenta :
+                vRed.setProgress(255);
+                vGreen.setProgress(0);
+                vBlue.setProgress(255);
+                vAlpha.setProgress(128);
+                message.setText("Magenta");
                 break;
 
             case R.id.iteYellow :
+                vRed.setProgress(255);
+                vGreen.setProgress(255);
+                vBlue.setProgress(0);
+                vAlpha.setProgress(128);
+                message.setText("Yellow");
                 break;
 
             case R.id.iteReset :
+                vRed.setProgress(0);
+                vGreen.setProgress(0);
+                vBlue.setProgress(0);
+                vAlpha.setProgress(0);
                 break;
 
             case R.id.iteAboutof :
                 //Go/jump to Aboutof Activity
-                Intent intent = new Intent(this, AboutofActivity.class);
-                startActivity(intent);
-                break;
+            Intent intent2 = new Intent(this, AboutofActivity.class);
+            startActivity(intent2);
+            break;
+            case  R.id.iteClose:
+                Palette.this.finish();
+                System.exit(0);
         }
         return super.onOptionsItemSelected(item);
     }
 
+
+    //#############################################
+    //CONTEXT MENU
+    //#############################################
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater2 = getMenuInflater();
+        inflater2.inflate(R.menu.menu2, menu);
+    }
+
+    @Override
+        public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.iteHelp :
+                Intent intent = new Intent(this,HelpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.iteRed :
+                vRed.setProgress(255);
+                vGreen.setProgress(0);
+                vBlue.setProgress(0);
+                vAlpha.setProgress(128);
+                break;
+            case R.id.iteReset:
+                vAlpha.setProgress(0);
+                vRed.setProgress(0);
+                vGreen.setProgress(0);
+                vBlue.setProgress(0);
+                break;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    //#############################################
+    //SEEKBARS
+    //#############################################
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean bo) {
         //1. Get Seekbar values
